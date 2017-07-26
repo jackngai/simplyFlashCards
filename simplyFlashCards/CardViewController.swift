34dec4e68b78d3c6c20a4cccfd2d04878ec0bebd
Jack Ngai <jackngai@gmail.com>
2017-07-26 11:19:18 -0400

    feat: Removed storyboard

---
+++ simplyFlashCards/CardViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CardViewController.swift
+//  simplyFlashCards
+//
+//  Created by Jack Ngai on 7/26/17.
+//  Copyright © 2017 Jack Ngai. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
