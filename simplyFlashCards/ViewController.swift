fb1aa28f84ac2abcc5fb932c993fb33a26cc6f68
Jack Ngai <jackngai@gmail.com>
2017-07-26 11:13:06 -0400

    Initial Commit

---
+++ simplyFlashCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simplyFlashCards
+//
+//  Created by Jack Ngai on 7/26/17.
+//  Copyright © 2017 Jack Ngai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
